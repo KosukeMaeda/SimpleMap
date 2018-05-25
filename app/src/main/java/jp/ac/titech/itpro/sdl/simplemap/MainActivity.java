eea4e0236d7b59dbc47ad0ed03cc8a530630822a
Kosuke Maeda <maeda.korosuke#gmail.com>
2018-05-25 15:33:49 +0900

    現在地を表示するボタンを設置
    10秒ごとに現在地に戻るのはなしに。

--- app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/simplemap/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +41,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     private enum UpdatingState {STOPPED, REQUESTING, STARTED}
 
     private UpdatingState state = UpdatingState.STOPPED;
+
+    private LatLng mLatLng;
 
     private final static String[] PERMISSIONS = {
             Manifest.permission.ACCESS_COARSE_LOCATION,
@@ -80,11 +83,18 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
                     LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
                     infoView.setText(getString(R.string.latlng_format,
                             latLng.latitude, latLng.longitude));
-                    if (googleMap != null)
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
+                    mLatLng = latLng;
                 }
             }
         };
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (googleMap != null)
+                    googleMap.animateCamera(CameraUpdateFactory.newLatLng(mLatLng));
+            }
+        });
     }
 
     @Override
